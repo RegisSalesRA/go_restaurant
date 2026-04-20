b76b40543c4903564a53c274ecb4c271d55e3daf
RegisSalesRA <regismsales@gmail.com>
2026-04-19 21:07:44 -0300

    users go router

--- routes/routes.go
+++ routes/routes.go
@@ -11,15 +11,21 @@ func RegisterRoutes(r *gin.Engine, pool *pgxpool.Pool) {
     // 1. Instancia os Repositories (Camada de Dados)
     cashRepo := repository.NewCashDrawerRepository(pool)
     categoriesRepo := repository.NewCategoriesRepository(pool)
+    usersRepo := repository.NewUsersRepository(pool)
 
     // 2. Instancia os Handlers (Camada de Controle)
     // Ajustei de 'handlers' para 'handler' para bater com seu import
     cashH := &handler.CashDrawerHandler{Repo: cashRepo}
     categoriesHandle := &handler.CategoriesHandler{Repo: categoriesRepo}
+    usersH := &handler.UsersHandler{Repo: usersRepo}
 
     // 3. Define as Rotas
     api := r.Group("/api/v1")
     {
+        // --- Rotas de Usuários (Públicas) ---
+        api.POST("/register", usersH.RegisterUser)
+        api.POST("/login", usersH.Login)
+        
         // Rotas de Caixa
         api.POST("/caixa/abrir", cashH.AbrirCaixaHandler)
         
@@ -30,5 +36,15 @@ func RegisterRoutes(r *gin.Engine, pool *pgxpool.Pool) {
         api.GET("/categories/:id", categoriesHandle.GetCategoryByID)
         api.PUT("/categories/:id", categoriesHandle.UpdateCategory)
         api.DELETE("/categories/:id", categoriesHandle.DeleteCategory)
+
+        // --- Rotas de Usuário (Protegidas ou Administrativas) ---
+        // Se você quiser listar todos os usuários ou ver um perfil específico
+        
+        usersGroup := api.Group("/users")
+        {
+            // usersGroup.Use(middleware.Auth()) 
+            usersGroup.GET("/", usersH.UsersList)
+            usersGroup.GET("/:id", usersH.GetUserByID)
+        }
     }
 }
